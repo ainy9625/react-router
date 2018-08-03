77e39859f9ea986412c162493ec6e9cd5be5d361
那宇-20235 <yu.na@hand-china.com>
2018-08-03 15:03:00 +0800

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link ,Switch ,Redirect} from "react-router-dom";
+import { HashRouter as Router, Route, Link ,Switch ,Redirect} from "react-router-dom";
 import './App.css';
 import Logo from './logo.svg'
 import Day from './components/day';
